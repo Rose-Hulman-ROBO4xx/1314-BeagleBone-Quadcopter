9a647d8b0c23be18e542dd261900adb4f70d9826
root <root@quad1.(none)>
2000-01-01 00:41:51 +0000

    simple complementary filter working

--- i2c_bitbang/pru_sw/example_apps/control_alg/control_alg.c
+++ i2c_bitbang/pru_sw/example_apps/control_alg/control_alg.c
@@ -2,29 +2,40 @@
 #include <stdlib.h>
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
+#include <math.h>
 
 #define PRU_NUM 	0
 #define PWM_N_ADDRESS 8
 #define PWM_E_ADDRESS 9
 #define PWM_S_ADDRESS 10
 #define PWM_W_ADDRESS 11
-
+#define ALPHA		.999
+#define BETA		1-ALPHA
+#define G		8192
 #define AM33XX
-
+#define CALIBRATION_SAMPLES 200
 volatile static void *pruDataMem;
 volatile static signed int *pruDataMem_int;
 
 
 typedef struct imu_data_t{
-	signed short x_a;
-	signed short y_a;
-	signed short z_a;
-	signed short x_g;
-	signed short y_g;
-	signed short z_g;
+	double x_a;
+	double y_a;
+	double z_a;
+	double x_g;
+	double y_g;
+	double z_g;
 	int sample_num;
 	
 }imu_data_t;
+
+typedef struct comp_filter_t {
+	double alpha;
+	double th;
+	double beta;
+	double g;
+} comp_filter_t;
+
 
 typedef struct pwm_frame_t{
 	volatile int* N;
@@ -32,6 +43,33 @@ typedef struct pwm_frame_t{
 	volatile int* S;
 	volatile int* W;
 } pwm_frame_t;
+
+
+void calibrate_imu_frame(imu_data_t * imu_frame, imu_data_t * calib_data);
+void filter_loop(imu_data_t * imu_frame, comp_filter_t * theta_p, comp_filter_t * theta_r, comp_filter_t * theta_y);
+void init_filter(comp_filter_t * comp_filter, double alpha, double beta, double g);
+void calculate_next_comp_filter(comp_filter_t * prev_data, double acc, double gyro, double dt);
+void get_imu_frame(imu_data_t * imu_frame);
+pwm_frame_t * get_pwm_pointer();
+void initialize_pru();
+void start_pru();
+void uninitialize_pru();
+imu_data_t * get_calibration_data();
+
+
+void init_filter(comp_filter_t * comp_filter, double alpha, double beta, double g){
+	comp_filter->alpha = alpha;
+	comp_filter->beta = beta;
+	comp_filter->g = g;
+	comp_filter->th = 0;
+}
+
+
+void calculate_next_comp_filter(comp_filter_t * prev_data, double acc, double gyro, double dt){
+	gyro = (gyro/32768.0f)*1000.0f;
+	printf("%f\n", gyro);
+	prev_data->th = prev_data->alpha*(prev_data->th + gyro*dt) + prev_data->beta*asin(acc/prev_data->g)*57.2957795f;
+}
 
 void get_imu_frame(imu_data_t * imu_frame){
 	while(!pruDataMem_int[1]){ // wait for pru to signal that new data is available
@@ -105,42 +143,90 @@ void uninitialize_pru(){
 
 }
 
+imu_data_t * get_calibration_data(){
+	imu_data_t * calib_data = malloc(sizeof(imu_data_t));
+	imu_data_t imu_data;
+	calib_data->x_a = 0;
+	calib_data->y_a = 0;
+	calib_data->z_a = 0;
+	calib_data->x_g = 0;
+	calib_data->y_g = 0;
+	calib_data->z_g = 0;
+	
+	int i;
+	printf("clearing DLPF on imu..\n");
+	for (i = 0; i < 200; i++){
+		get_imu_frame(&imu_data);
+	}
+
+	for (i = 0; i < CALIBRATION_SAMPLES; i++){
+		get_imu_frame(&imu_data);
+		
+//		printf("%f, %f, %f, %f, %f, %f\n", calib_data->x_a, calib_data->y_a, calib_data->z_a, calib_data->x_g, calib_data->y_g, calib_data->z_g);
+		calib_data->x_a += imu_data.x_a;
+		calib_data->y_a += imu_data.y_a;
+		calib_data->z_a += imu_data.z_a;
+
+		calib_data->x_g += imu_data.x_g;
+		calib_data->y_g += imu_data.y_g;
+		calib_data->z_g += imu_data.z_g;
+	}
+	calib_data->x_a /= CALIBRATION_SAMPLES;
+	calib_data->y_a /= CALIBRATION_SAMPLES;
+	calib_data->z_a /= CALIBRATION_SAMPLES;
+	calib_data->x_g /= CALIBRATION_SAMPLES;
+	calib_data->y_g /= CALIBRATION_SAMPLES;
+	calib_data->z_g /= CALIBRATION_SAMPLES;
+	return calib_data;
+}
+
 int main (void)
 {
-    
-    initialize_pru();
-    int count = 0; 
-    pwm_frame_t * pwm_frame = get_pwm_pointer();
+	initialize_pru();
+	start_pru();
+	pwm_frame_t * pwm_frame = get_pwm_pointer();
+	*(pwm_frame->N) = 100000;
+	*(pwm_frame->E) = 100000;
+	*(pwm_frame->S) = 100000;
+	*(pwm_frame->W) = 100000;
 
-    *(pwm_frame->N) = 100000;
-    *(pwm_frame->E) = 100000;
-    *(pwm_frame->S) = 100000;
-    *(pwm_frame->W) = 100000;
-    imu_data_t imu_data;
-    start_pru();
-    int i;
-    printf("clearing DLPF on imu..\n");
-    for (i = 0; i < 200; i++){
-	    get_imu_frame(&imu_data);
-    }
-    double x_g_avg = 0;
-    for (i = 0; i < 2000; i++){
-        get_imu_frame(&imu_data);
-	x_g_avg += imu_data.x_g;
-        printf("%d: %d\n", imu_data.sample_num, imu_data.x_g);
-        count += 1;
-    }
-    x_g_avg/=2000;
-    double current_rot = 0;
+	imu_data_t * calib_data;
+	calib_data = get_calibration_data();
+	printf("check calibration data for sanity: %f, %f, %f, %f, %f, %f\n", calib_data->x_a, calib_data->y_a, calib_data->z_a, calib_data->x_g, calib_data->y_g, calib_data->z_g);
+	
+	comp_filter_t * theta_p = malloc(sizeof(comp_filter_t));
+	comp_filter_t * theta_r = malloc(sizeof(comp_filter_t));
+	comp_filter_t * theta_y = malloc(sizeof(comp_filter_t));
+	imu_data_t * imu_frame = malloc(sizeof(comp_filter_t));
+	
+	init_filter(theta_p, ALPHA, BETA, G);
+	init_filter(theta_r, ALPHA, BETA, G);
 
-    for (i = 0; i < 5000000; i++){
-        get_imu_frame(&imu_data);
-	current_rot += imu_data.x_g-x_g_avg;
-        *(pwm_frame->N) = 130000-(current_rot/30);
-	printf("current_rot: %d\n", *(pwm_frame->N));
-    }
-    printf("%d\n", count);
-    uninitialize_pru();
-    return(0);
+	
+	while(1){
+		
+		get_imu_frame(imu_frame);
+		calibrate_imu_frame(imu_frame, calib_data);
+		filter_loop(imu_frame, theta_p, theta_r, theta_y);
+		printf("pitch: %f, roll: %f\n", theta_p->th, theta_r->th);
+	}
+
+	uninitialize_pru();
+	return(0);
+}
+
+
+void calibrate_imu_frame(imu_data_t * imu_frame, imu_data_t * calib_data){
+	imu_frame->x_a -= calib_data->x_a;
+	imu_frame->y_a -= calib_data->y_a;
+	imu_frame->z_a -= calib_data->z_a;
+
+	imu_frame->x_g -= calib_data->x_g;
+	imu_frame->y_g -= calib_data->y_g;
+	imu_frame->z_g -= calib_data->z_g;
+}
+void filter_loop(imu_data_t * imu_frame, comp_filter_t * theta_p, comp_filter_t * theta_r, comp_filter_t * theta_y){
+	calculate_next_comp_filter(theta_p, imu_frame->y_a, -imu_frame->x_g, 1/250.0f);
+	calculate_next_comp_filter(theta_r, imu_frame->x_a, -imu_frame->y_g, 1/250.0f);
 }
 
